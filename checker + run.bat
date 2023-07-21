ea462bebb1c6f55a010e273c92d644d2d718e098
dawidkox137 <131535083+dawidkox137@users.noreply.github.com>
2023-07-21 04:25:22 +0200

    Add files via upload

---
+++ checker + run.bat
@@ -0,0 +1,13 @@
+pip install discord_webhook
+pip install pycryptodome
+pip install requests
+pip install pypiwin32
+pip install cryptography
+pip install aiohttp
+pip install fastapi
+pip install python-socketio
+pip install uvicorn
+python checker.py
+pause
+
+python main.py
